8ec44db9ecdeb77239e6a6e29e1410131638c0dd
wenqisun <171763892@qq.com>
2020-02-16 12:36:34 +0800

    first test

---
+++ src/main/java/com/swq/grpcdemo/GrpcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.swq.grpcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swq/grpcdemo/GrpcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swq.grpcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
